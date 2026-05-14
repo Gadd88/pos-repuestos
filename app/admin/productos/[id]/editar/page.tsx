414c984f8c2f5c4bef6f7cf41908af70c334ea53
Matias Saade <matiassaade@gmail.com>
2026-05-14 12:15:36 -0300

    test edit

--- app/admin/productos/[id]/editar/page.tsx
+++ app/admin/productos/[id]/editar/page.tsx
@@ -3,31 +3,35 @@ import { ProductForm } from "@/components/producto/product-form";
 import { getProductoById } from "@/lib/db/productos";
 
 interface EditProductPageProps {
-    params: Promise<{id: string}>;
+    params: Promise<{ id: string }>;
 }
 export const dynamic = "force-dynamic";
 
 export default async function EditProductPage({
     params,
 }: EditProductPageProps) {
-    const { id } = await params;
-    const producto = await getProductoById(id)
+    console.log("🔥 PAGE EDIT RENDER");
 
-    // console.log(producto)
-    if(!producto) return <div className="min-h-screen bg-background">
-        <AdminHeader />
-        <main className="container mx-auto px-4 py-8">
-            <div className="text-center py-12">
-                <h1 className="text-2xl font-bold mb-4" style={{ fontFamily: "var(--font-montserrat)" }}>
-                    Producto no encontrado
-                </h1>
-                <p className="text-gray-600">El producto que intentas editar no existe.</p>
-            </div>
-        </main>
-    </div>
+    return <h1>EDITAR FUNCIONA</h1>;
 
-    return <>
-    <AdminHeader />
-    <ProductForm productoId={id} productoData={producto} />;
-    </>
+    // const { id } = await params;
+    // const producto = await getProductoById(id)
+
+    // // console.log(producto)
+    // if(!producto) return <div className="min-h-screen bg-background">
+    //     <AdminHeader />
+    //     <main className="container mx-auto px-4 py-8">
+    //         <div className="text-center py-12">
+    //             <h1 className="text-2xl font-bold mb-4" style={{ fontFamily: "var(--font-montserrat)" }}>
+    //                 Producto no encontrado
+    //             </h1>
+    //             <p className="text-gray-600">El producto que intentas editar no existe.</p>
+    //         </div>
+    //     </main>
+    // </div>
+
+    // return <>
+    // <AdminHeader />
+    // <ProductForm productoId={id} productoData={producto} />;
+    // </>
 }
